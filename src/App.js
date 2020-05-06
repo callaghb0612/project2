ae3b03d60e27c74576c8ee3d59d23d907141f9cb
Bryant Callaghan <callaghb0612@uwec.edu>
2020-05-06 19:58:13 +0000

    start of site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="homepage">
+      <div id="header">Know-A-Bunch</div>
+      <div id="menu"></div>
+    </div>
+  );
+}
+
+export default App;
