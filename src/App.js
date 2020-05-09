2cecf20138e05f0234f603210230af2a718c2f05
Bryant Callaghan <callaghb0612@uwec.edu>
2020-05-09 17:59:52 -0500

    start of quiz builder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {usestate} from 'react';
 import './App.css';
+import { QuizBuilder } from './QuizBuilder';
+
+function loadBuilder(){
+  this.usestate
+}
 
 function App() {
   return (
-    <div id="homepage">
-      <div id="header">Know-A-Bunch</div>
-      <div id="menu"></div>
+    <div id="root">
+      <div id="header"><span id="title">Know-A-Bunch</span><span id="add-button" onClick={() => loadBuilder()}>+</span></div>
+      <div id="content">
+        <div id="content-builder" hidden>
+        {QuizBuilder()}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/question.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './question.css';
+
+export function question(){
+    return (
+        
+    );
+}
